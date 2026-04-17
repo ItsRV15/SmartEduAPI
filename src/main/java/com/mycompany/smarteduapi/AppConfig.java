953fb026a6cd47a6f9fe7a6cf54cb697bbc85410
ItsRV15 <ravinduranasinghe15@gmail.com>
2026-04-17 11:13:43 +0530

    Created the AppConfig class for configuration

---
+++ src/main/java/com/mycompany/smarteduapi/AppConfig.java
@@ -0,0 +1,19 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smarteduapi;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Ravindu Ranasinghe
+ */
+@ApplicationPath("/api/v1")
+public class AppConfig extends Application {
+    
+    
+}
