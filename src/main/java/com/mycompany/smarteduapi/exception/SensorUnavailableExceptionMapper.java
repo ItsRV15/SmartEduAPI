d6f30742c5c9eeb672929974e45bb1a724fb2d77
ItsRV15 <ravinduranasinghe15@gmail.com>
2026-04-21 13:02:36 +0530

    Created the mapper class for the sensor unavailable

---
+++ src/main/java/com/mycompany/smarteduapi/exception/SensorUnavailableExceptionMapper.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smarteduapi.exception;
+
+import com.mycompany.smarteduapi.model.ErrorMessage;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Ravindu Ranasinghe
+ */
+@Provider
+public class SensorUnavailableExceptionMapper implements ExceptionMapper<SensorUnavailableException> {
+
+    @Override
+    public Response toResponse(SensorUnavailableException exception) {
+
+        ErrorMessage error = new ErrorMessage(
+                exception.getMessage(),
+                403
+        );
+
+        return Response.status(Response.Status.FORBIDDEN)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
