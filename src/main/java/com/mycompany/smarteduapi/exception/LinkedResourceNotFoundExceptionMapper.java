710c814809c994629c10e1309e8c859fc68f4cf5
ItsRV15 <ravinduranasinghe15@gmail.com>
2026-04-21 11:54:41 +0530

    Created mapper for the invalid room

---
+++ src/main/java/com/mycompany/smarteduapi/exception/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smarteduapi.exception;
+
+
+import com.mycompany.smarteduapi.model.ErrorMessage;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Ravindu Ranasinghe
+ */
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+    
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException exception) {
+
+        ErrorMessage error = new ErrorMessage(
+                exception.getMessage(),
+                422
+        );
+
+        return Response.status(422)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+    
+    
+    
+}
